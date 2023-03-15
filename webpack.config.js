c20ab203dea2b5e39312d0d9af194aca365474d3
Julen Alvaro <julenalvaro@gmail.com>
2023-03-15 13:46:07 +0100

    clase 13

--- webpack.config.js
+++ webpack.config.js
@@ -3,17 +3,26 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const miniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 /** @type {import('webpack').Configuration} */
 module.exports = {
   entry: './src/index.js',                    //punto de entrada de la aplicación, a partir de aquí se va a generar el bundle
   output: {
     path: path.resolve(__dirname, 'dist'),   // establece la ubicación del archivo de salida (el bundle) generado por     Webpack en un directorio específico (dist) dentro del proyecto.
-    filename: 'main.js',
+    filename: '[name].[contenthash].js',     // establece el nombre del archivo de salida (el bundle) generado por Webpack.
     assetModuleFilename: 'assets/images/[hash][ext][query]',
   },
   resolve: {
-    extensions: ['.js'],                      // extensiones que va a leer webpack
+    extensions: ['.js'],                // extensiones que va a leer webpack
+    alias: {
+      '@utils': path.resolve(__dirname, 'src/utils/'), // alias para importar archivos
+      '@templates': path.resolve(__dirname, 'src/templates/'),
+      '@styles': path.resolve(__dirname, 'src/styles/'),
+      '@images': path.resolve(__dirname, 'src/assets/images/'),
+      '@fonts': path.resolve(__dirname, 'src/assets/fonts/'),
+    }
   },
   module:{
     rules:[
@@ -38,18 +47,11 @@ module.exports = {
               type: 'asset/resource',
             },
             {
-              test: /\.(woff|woff2)$/,
-              use: {
-                loader: 'url-loader',
-                options: {
-                  limit: 10000,
-                  mimetype: "application/font-woff",
-                  name: "[name].[ext]",
-                  outputPath: "./assets/fonts/",
-                  publicPath: "./assets/fonts/",
-                  esModule: false,
-                },
-              }
+              test: /\.(woff|woff2|eot|ttf|otf)$/i,
+              type: "asset/resource",
+              generator: {
+                filename: "assets/fonts/[hash][ext]",
+              },
             }
           ]
   },
@@ -59,7 +61,10 @@ module.exports = {
       template: './public/index.html',
       filename: './index.html',
     }),
-    new miniCssExtractPlugin(),
+    new miniCssExtractPlugin({
+      filename: 'assets/[name].[contenthash].css'
+    }
+    ),
     new CopyPlugin({
       patterns: [
         {
@@ -68,5 +73,12 @@ module.exports = {
         },
       ]
     }),
-  ]
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin(),
+      new TerserPlugin(),
+    ],
+  },
 };
